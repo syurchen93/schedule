e4066f05d15b9534a59294767d1540508cfaa8a9
sergei.iurchenko <sergei.iurchenko@check24.de>
2024-02-25 14:05:13 +0100

    feat: added Competition foreign key to Competition model

--- model/league/competition.go
+++ model/league/competition.go
@@ -10,6 +10,8 @@ const (
 
 type Competition struct {
 	ID uint `gorm:"primaryKey"`
+	CountryID uint `gorm:"not null"`
+	Country Country `gorm:"foreignKey:CountryID"`
 	Name string `gorm:"type:varchar(256); not null"`
 	Type string `gorm:"type:enum('league', 'cup'); not null"`
 	Logo string `gorm:"type:varchar(256); not null"`
