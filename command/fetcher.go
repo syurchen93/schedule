5e13eb5703c54f4b456953fc5c1e50a39c80c021
sergei.iurchenko <sergei.iurchenko@check24.de>
2024-04-13 15:25:02 +0200

    feat: add country fetcher

--- command/fetcher.go
+++ command/fetcher.go
@@ -3,14 +3,50 @@ package main
 import (
 	"github.com/syurchen93/api-football-client/client"
 	"github.com/syurchen93/api-football-client/request/league"
+	"github.com/syurchen93/api-football-client/response/leagues"
 
+	"fmt"
+	"gorm.io/gorm"
+
+	"schedule/db"
+	model "schedule/model/league"
 	"schedule/util"
+	transformer "schedule/util/transformer/fetcher"
 )
 
+var apiClient *client.Client
+var dbGorm *gorm.DB
+
 func main() {
-	apiClient := client.NewClient(util.GetEnv("API_FOOTBALL_KEY"))
+	apiClient = client.NewClient(util.GetEnv("API_FOOTBALL_KEY"))
+	db.Init()
+	dbGorm = db.Db()
+
+	fetchAndPersistCountries()
+}
+
+func fetchAndPersistCountries() {
+	var errorCount int
+	var createdCount int
 
 	getCountriesRequest := league.Country{}
 
-	apiClient.DoRequest(getCountriesRequest)
+	countryResponses, err := apiClient.DoRequest(getCountriesRequest)
+	if err != nil {
+		fmt.Println(err)
+	}
+	dbGorm.Delete(model.Country{}, "id > 0")
+
+	for _, countryResponse := range countryResponses {
+		country := transformer.CreateCountryFromResponse(countryResponse.(leagues.Country))
+		result := db.Db().Create(&country)
+		if result.Error != nil {
+			errorCount++
+		}
+		if result.RowsAffected > 0 {
+			createdCount++
+		}
+	}
+
+	fmt.Printf("Countries Created: %d, Error: %d\n", createdCount, errorCount)
 }
