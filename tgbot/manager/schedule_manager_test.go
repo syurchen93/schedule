ef839f0486c48bc7ec7cfb5aab44b01004ede2fb
sergei.iurchenko <sergei.iurchenko@check24.de>
2024-05-31 18:34:35 +0200

    feat: score string test

--- tgbot/manager/schedule_manager_test.go
+++ tgbot/manager/schedule_manager_test.go
@@ -1,11 +1,12 @@
 package manager
 
 import (
-	"github.com/stretchr/testify/assert"
-	"testing"
-
 	"schedule/model"
 	"schedule/model/league"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/syurchen93/api-football-client/common"
 )
 
 func Test_getCompetitionStandings(t *testing.T) {
@@ -27,6 +28,26 @@ func Test_getCompetitionStandings(t *testing.T) {
 	assert.Equal(t, 1, standingDatas[0].Standings[0].Position)
 	assert.Equal(t, "Team A2", standingDatas[0].Standings[1].TeamName)
 	assert.Equal(t, 2, standingDatas[0].Standings[1].Position)
+}
+
+func Test_generateScoreString(t *testing.T) {
+	fixture := league.Fixture{
+		Status: common.NotStarted,
+	}
+	assert.Equal(t, "", generateScoreString(fixture))
+
+	fixture.Status = common.Finished
+	fixture.GoalsHome = 2
+	fixture.GoalsAway = 1
+	assert.Equal(t, "2 : 1", generateScoreString(fixture))
+
+	fixture.Status = common.FinishedAfterExtra
+	assert.Equal(t, "2 : 1 (ET)", generateScoreString(fixture))
+
+	fixture.Status = common.FinishedAfterPenalty
+	fixture.PenaltyHome = 4
+	fixture.PenaltyAway = 3
+	assert.Equal(t, "2 : 1 P(4 : 3)", generateScoreString(fixture))
 }
 
 func createStandingModel(teamName string, position int, groupName string) league.Standing {
