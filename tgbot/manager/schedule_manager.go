f837517582c6e865c2808845fc013595e1d69c3f
sergei.iurchenko <sergei.iurchenko@check24.de>
2024-08-08 09:53:47 +0200

    fix: nil pointer exception

--- tgbot/manager/schedule_manager.go
+++ tgbot/manager/schedule_manager.go
@@ -176,17 +176,26 @@ func CreateCompetitionFixtureViewFromAlers(alerts []bot.Alert) []CompetitionView
 }
 
 func createFixtureView(fixture league.Fixture, user *bot.User) FixtureView {
+	var homeTeamCode, awayTeamCode *string
+
 	userTime, err := time.LoadLocation(user.Timezone)
 	if err != nil {
 		userTime = time.UTC
 	}
+	if fixture.HomeTeam.Code != nil {
+		homeTeamCode = fixture.HomeTeam.Code
+	}
+	if fixture.AwayTeam.Code != nil {
+		awayTeamCode = fixture.AwayTeam.Code
+	}
+
 	return FixtureView{
 		ID:            fixture.ID,
 		HomeTeamName:  fixture.HomeTeam.Name,
-		HomeTeamCode:  *fixture.HomeTeam.Code,
+		HomeTeamCode:  *homeTeamCode,
 		IsHomeUserFav: fixture.HomeTeam.IsUserFav,
 		AwayTeamName:  fixture.AwayTeam.Name,
-		AwayTeamCode:  *fixture.AwayTeam.Code,
+		AwayTeamCode:  *awayTeamCode,
 		IsAwayUserFav: fixture.AwayTeam.IsUserFav,
 		Date:          fixture.Date.In(userTime),
 		Score:         generateScoreString(fixture),
